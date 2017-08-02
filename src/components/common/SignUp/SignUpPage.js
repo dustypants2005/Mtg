ee15c10907daf3d74b745a6a5f74341567518a5d
Dustin <dustypants2005@hotmail.com>
2017-08-02 17:06:36 -0400

    add login and signup

---
+++ src/components/common/SignUp/SignUpPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class SignUpPage extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+		}
+	}
+	render() {
+		return (
+		<div>Sign Up</div>
+		);
+	}
+}
+
+export default SignUpPage;
